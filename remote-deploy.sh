1c3d40bc60c2beb43f3588b766ac4395ad1d7686
Charlie Somerville <charlie@charliesomerville.com>
2014-02-15 20:11:54 +1100

    add remote-deploy.sh

---
+++ remote-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This script runs on the net-metrics host in a checkout of the repo.
+#
+# It is responsible for performing a 'deploy'. At this point that basically
+# consists of restarting some daemons.
+#
+set -ex
+
+echo "hello!"
