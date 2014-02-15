e755ec57c873b62ee678eaaf12e1a0d2df0f0218
Charlie Somerville <charlie@charliesomerville.com>
2014-02-15 21:59:51 +1100

    ping at an interval of 10 seconds

--- collector/collector.rb
+++ collector/collector.rb
@@ -9,9 +9,9 @@ config = YAML.load_file(File.expand_path("config.yml", __dir__))
 mysql = Mysql2::Client.new(config["mysql"])
 queue = Queue.new
 
-worker_threads = config["ping_hosts"].map { |host|
+worker_threads = config["ping"]["hosts"].map { |host|
   Thread.new do
-    io = IO.popen(["ping", host])
+    io = IO.popen(["ping", "-i", config["ping"]["interval"].to_s, host])
     while line = io.gets
       if /time=([0-9.]+) ms/ =~ line
         queue << Datapoint.new(host, $1.to_f.round)
